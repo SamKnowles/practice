545e980c6234434f6ec9334d598a9e82c0844407
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-06 20:01:55 -0700

    preflop word alterations

--- src/Components/PreFlop.js
+++ src/Components/PreFlop.js
@@ -4,10 +4,10 @@ function PreFlop() {
     return (
         <div>
         <div className="preFlop-container">
-                <div className="item1">1</div>
+                <div className="item1">PRE-FLOP</div>
                 <div className="item2">2</div>
                 <div className="item3">
-                When you are sitting at the poker table, mental energy is a precious resource. It is not reasonable to expect that you will observe and fully integrate every detail at the table. Part of improving as a poker player is learning to come to the table with greater reserves of mental energy, but far more importantly is the skill to conserve mental energy.
+                {`When you are sitting at the poker table, mental energy is a precious resource. It is not reasonable to expect that you will observe and fully integrate every detail at the table. Part of improving as a poker player is learning to come to the table with greater reserves of mental energy, but far more importantly is the skill to conserve mental energy.
 
 So how can we conserve mental energy without missing key information. For example, if we decided to tune out every 5th hand or get up from the table 5-10 minutes every hour, we risk missing key information. This isn’t to say that breaks aren’t important. I highly recommend taking at least some sort of break every hour and probably taking some sort of one hand mental break every half hour.
 
@@ -17,201 +17,204 @@ What can we do to increase our efficiency of observation at the poker table? One
 
 Now quickly, a guide to using this checklist. These questions apply for each opponent in your game. It’s a good idea to use stereotypes and player-pool tendencies to a certain extent, but keep an eye out because people can surprise you if you aren’t looking closely. Sometimes the answer to these questions don’t have a clear implication. For example, if a player has multiple raise sizes preflop, that doesn’t necessarily tell you what those raise sizes mean. It is still worth observing so that you can try and then observe what those raise sizes mean now that you have discovered their existence. Not everything worth observing is on this checklist, so don’t let this become the be all end all of your poker observation. Finally, people can change, so be ready to reevaluate and always consider game circumstances when making your reads.
 
-General:
+`}
 
+<text style={{fontWeight: "bold"}}>General: </text>
+
+{`
 Buy-in habits
 
-Does this player buy in for the max?
+- Does this player buy in for the max?
 
-Does this player buy in for at least 100 bb?
+- Does this player buy in for at least 100 bb?
 
-Does this player top off regularly?
+- Does this player top off regularly?
 
-Does this player reload?
+- Does this player reload?
 
 Conversation
 
-Does this player talk strategy at the table?
+- Does this player talk strategy at the table?
 
-Does this player claim to be a professional?
+- Does this player claim to be a professional?
 
-Does this player say dumb things like “I knew you had that hand that beat me” or “I should have called with 83 offsuit since i would have turned two pair” (in drunken breath right into your face)?
+- Does this player say dumb things like “I knew you had that hand that beat me” or “I should have called with 83 offsuit since i would have turned two pair” (in drunken breath right into your face)?
 
 Other general concerns
 
-Is this player drinking? A lot or casually?
+- Is this player drinking? A lot or casually?
 
-Does this player know all of the dealers and half of the table?
+- Does this player know all of the dealers and half of the table?
 
-Does this player tilt? Is this player tilting?
+- Does this player tilt? Is this player tilting?
 
 Preflop:
 
 Limping
 
-Does this player always limp preflop?
+- Does this player always limp preflop?
 
-Does this player ever limp preflop?
+- Does this player ever limp preflop?
 
-What types of hands does this player limp?
+- What types of hands does this player limp?
 
-Does this player always limp in early position?
+- Does this player always limp in early position?
 
-Does this player normally limp-call or just limp to try and see a cheap flop?
+- Does this player normally limp-call or just limp to try and see a cheap flop?
 
-Does this player always limp-call if other players have limp-called in front of them?
+- Does this player always limp-call if other players have limp-called in front of them?
 
 Raise sizes
 
-Does this player use multiple raise sizes independent of limpers in the pot?
+- Does this player use multiple raise sizes independent of limpers in the pot?
 
-What do those raise sizes indicate?
+- What do those raise sizes indicate?
 
 3-betting
 
-Will this player ever 3-bet?
+- Will this player ever 3-bet?
 
-Does this player ever 3-bet light?
+- Does this player ever 3-bet light?
 
-Does this player 3-bet hands like AK or only premium pairs?
+- Does this player 3-bet hands like AK or only premium pairs?
 
 4-betting
 
-Does this player ever 4-bet?
+- Does this player ever 4-bet?
 
-Does this player ever 4-bet light?
+- Does this player ever 4-bet light?
 
 Stealing
 
-Does this player take obvious squeezing spots?
+- Does this player take obvious squeezing spots?
 
-Does this player play aggressively preflop against players you are perceiving as having wide/weak preflop ranges?
+- Does this player play aggressively preflop against players you are perceiving as having wide/weak preflop ranges?
 
 General Postflop
 
 Bluffing
 
-Does this player ever bluff… ever?
+- Does this player ever bluff… ever?
 
-Does this player regularly bet into weakness?
+- Does this player regularly bet into weakness?
 
-Does this player have obvious timing tells?
+- Does this player have obvious timing tells?
 
-What do those tells mean?
+- What do those tells mean?
 
-Does this player have obvious bet sizing tells?
+- Does this player have obvious bet sizing tells?
 
-What do those tells mean?
+- What do those tells mean?
 
-Will this player ever check raise?
+- Will this player ever check raise?
 
-What types of hand will this player check-raise?
+- What types of hand will this player check-raise?
 
 Semi-bluffing/Draw-play
 
-Does this player ever semi-bluff?
+- Does this player ever semi-bluff?
 
-Does this player always semi-bluff?
+- Does this player always semi-bluff?
 
-Does this player chase draws with bad prices?
+- Does this player chase draws with bad prices?
 
 Slow-Playing
 
-Does this player ever slowplay?
+- Does this player ever slowplay?
 
-Does this player ever slowplay on coordinated boards?
+- Does this player ever slowplay on coordinated boards?
 
 Hand Strength considerations
 
-Does this player overvalue hands by calling too wide?
+- Does this player overvalue hands by calling too wide?
 
-Does this player value bet thinly? On purpose?
+- Does this player value bet thinly? On purpose?
 
-Does this player overvalue hands by absolute strength?
+- Does this player overvalue hands by absolute strength?
 
-Can this player fold an overpair?
+- Can this player fold an overpair?
 
 Other considerations
 
-Does this player seem to understand pot odds?
+- Does this player seem to understand pot odds?
 
-Does this player play very face up in 3-bet pots?
+- Does this player play very face up in 3-bet pots?
 
 Flop
 
 Continuation betting
 
-How often is this player continuation betting?
+- How often is this player continuation betting?
 
-Does this player continuation bet air?
+- Does this player continuation bet air?
 
-Does this player continuation bet marginal hands?
+- Does this player continuation bet marginal hands?
 
-Does this player continuation bet differently in position vs out of position?
+- Does this player continuation bet differently in position vs out of position?
 
 Donk betting
 
-Does this player ever donk bet?
+- Does this player ever donk bet?
 
-What kinds of hands does this player donk bet? Have they folded to flop raises? Do they continue on the turn?
+- What kinds of hands does this player donk bet? Have they folded to flop raises? Do they continue on the turn?
 
 Other flop considerations
 
-Does this player always call continuation bets with bottom pair+?
+- Does this player always call continuation bets with bottom pair+?
 
-Does this player always call continuation bets with draws?
+- Does this player always call continuation bets with draws?
 
-Does this player call differently in position vs out of position?
+- Does this player call differently in position vs out of position?
 
-Does this player ever check-raise flops?
+- Does this player ever check-raise flops?
 
-Is this ever a bluff or semi-bluff?
+- Is this ever a bluff or semi-bluff?
 
-Does this player attack continuation bets on weak, uncoordinated boards?
+- Does this player attack continuation bets on weak, uncoordinated boards?
 
-Will this player float in position?
+- Will this player float in position?
 
 Turn
 
 Continuation bets
 
-How often does this player bet the turn after continuation betting the flop?
+- How often does this player bet the turn after continuation betting the flop?
 
 Calling/raising
 
-Does this player overfold the turn after calling the flop very widely?
+- Does this player overfold the turn after calling the flop very widely?
 
 Other turn considerations
 
-Does this player like to pot control in position on the turn with top pair hands?
+- Does this player like to pot control in position on the turn with top pair hands?
 
-Does this player generally continue semibluffs in position? Out of position?
+- Does this player generally continue semibluffs in position? Out of position?
 
 River
 
 Bluffing
 
-Has this player ever bluffed the river?
+- Has this player ever bluffed the river?
 
-Has the player ever bluff check-raised the river?
+- Has the player ever bluff check-raised the river?
 
 Donk-Betting
 
-Does this player tend to donk bet after calling two streets?
+- Does this player tend to donk bet after calling two streets?
 
-Are these with value hands? Missed draws?
+- Are these with value hands? Missed draws?
 
 Pot controlling
 
-How thinly does this player value bet?
+- How thinly does this player value bet?
 
-Will this player raise non-nutted hands for value?
+- Will this player raise non-nutted hands for value?
 
-Post-Hand considerations
+- Post-Hand considerations
 
-Does this player generally fast-roll?
+- Does this player generally fast-roll?
 
-Will this player tell you their hand/talk poker with you after the hand?
+- Will this player tell you their hand/talk poker with you after the hand?`}
                 </div>
                 <div className="item4">4</div>
                 <div className="item5">5</div>
