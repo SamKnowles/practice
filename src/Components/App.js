423cc62357a54c87a9881f4e4e23799004a403e2
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-09 20:02:40 -0700

    added table component

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,15 +2,17 @@ import React from 'react'
 import Landing from './Landing'
 import PreFlop from './PreFlop'
 import Maths from './Maths'
+import Table from './Table'
 
 import '../Styles/PreFlop.css';
 
 function App() {
     return (
         <div>
-            <Landing></Landing>
-            <PreFlop></PreFlop>
-            <Maths></Maths>
+            <Landing />
+            <PreFlop />
+            <Maths />
+            <Table />
         </div>
     )
 }
