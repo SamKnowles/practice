0c697713d6090a5ac8fa8ec068127f081036f078
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-09 20:27:10 -0700

    added navbar and router capability

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom';
+
+import Nav from './Nav'
 import Landing from './Landing'
 import PreFlop from './PreFlop'
 import Maths from './Maths'
 import Table from './Table'
 
 import '../Styles/PreFlop.css';
+import '../Styles/Table.css';
+import '../Styles/Nav.css';
 
 function App() {
     return (
-        <div>
-            <Landing />
-            <PreFlop />
-            <Maths />
-            <Table />
+        <div className='app-wrapper'>
+            <Nav />
+            <Switch>
+                <Route exact path='/' component={Table}></Route>
+                <Route path='/landing' compontnet={Landing} ></Route>
+                <Route path='/PreFlop' component={PreFlop}></Route>
+                <Route path='/Maths' component={Maths}></Route>
+            </Switch>
         </div>
     )
 }
