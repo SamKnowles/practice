43a0de240ce41606eae3f6e0676a8feed7d76347
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-09 20:51:33 -0700

    added selected class to nav

---
+++ src/Components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Nav() {
+    return (
+        <div className='nav-container'>
+            <NavLink className='nav-item' activeClassName="selcted" to='/'>Table</NavLink>
+            <NavLink className='nav-item' activeClassName="selcted" to='/landing'>Landing</NavLink>
+            <NavLink className='nav-item' activeClassName="selcted" to='/preflop'>Pre-Flop</NavLink>
+            <NavLink className='nav-item' activeClassName="selcted" to='/maths'>Maths</NavLink>
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/Components/Table.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function TAble() {
+    return (
+        <div>
+            <div class="table-container">
+                <div class="table-item1">1</div>
+                <div class="table-item2">2</div>
+                <div class="table-item3">3</div>
+                <div class="table-item4">4</div>
+                <div class="table-item5">5</div>
+                <div class="table-item6">6</div>
+                <div class="table-item7">7</div>
+                <div class="table-item8">8</div>
+                <div class="table-item9">9</div>
+                <div class="table-item10">10</div>
+                <div class="table-item11">11</div>
+                <div class="table-item12">12</div>
+                <div class="table-item13">13</div>
+                <div class="table-item14">14</div>
+                <div class="table-item15">15</div>
+                <div class="table-item16">16</div>
+            </div>
+        </div>
+    )
+}
+
+export default TAble
