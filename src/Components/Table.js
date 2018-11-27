bd5b340058cf205917fe54139f22160407cdacab
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-27 15:40:54 -0700

    added css to table player seats

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -7,24 +7,64 @@ function Table() {
                 <div className='seating-wrapper'>
                     <div className='seating'>
                         <div>1</div>
-                        <div>2</div>
-                        <div>3</div>
-                        <div>4</div>
+                        <div className='player-profile'>
+                            <div className='player-face'></div>
+                            <div className='player-cards'></div>
+                            <div className='player-chips'></div>
+                        </div>
+                        <div className='player-profile'>
+                            <div className='player-face'></div>
+                            <div className='player-cards'></div>
+                            <div className='player-chips'></div>
+                        </div>
+                        <div className='player-profile'>
+                            <div className='player-face'></div>
+                            <div className='player-cards'></div>
+                            <div className='player-chips'></div>
+                        </div>
                         <div>5</div>
-                        <div>6</div>
+                        <div className='player-profile'>
+                            <div className='player-face'></div>
+                            <div className='player-cards'></div>
+                            <div className='player-chips'></div>
+                        </div>
                         <div>7</div>
                         <div>8</div>
                         <div>9</div>
-                        <div>10</div>
-                        <div>11</div>
+                        <div className='player-profile'>
+                            <div className='player-face'></div>
+                            <div className='player-cards'></div>
+                            <div className='player-chips'></div>
+                        </div>
+                        <div className='player-profile'>
+                            <div className='player-face'></div>
+                            <div className='player-cards'></div>
+                            <div className='player-chips'></div>
+                        </div>
                         <div>13</div>
                         <div>14</div>
                         <div>15</div>
-                        <div>16</div>
+                        <div className='player-profile'>
+                            <div className='player-face'></div>
+                            <div className='player-cards'></div>
+                            <div className='player-chips'></div>
+                        </div>
                         <div>17</div>
-                        <div>18</div>
-                        <div>19</div>
-                        <div>20</div>
+                        <div className='player-profile'>
+                            <div className='player-face'></div>
+                            <div className='player-cards'></div>
+                            <div className='player-chips'></div>
+                        </div>
+                        <div className='player-profile'>
+                            <div className='player-face'></div>
+                            <div className='player-cards'></div>
+                            <div className='player-chips'></div>
+                        </div>
+                        <div className='player-profile'>
+                            <div className='player-face'></div>
+                            <div className='player-cards'></div>
+                            <div className='player-chips'></div>
+                        </div>
                     </div>
                 </div>
             </div>
