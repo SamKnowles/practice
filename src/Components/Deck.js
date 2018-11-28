3a4450e35553db093fbe02d94fd80c3c46b0bd3e
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-27 17:56:05 -0700

    going a different derection with my deck

--- src/Components/Deck.js
+++ src/Components/Deck.js
@@ -1,64 +1,64 @@
 
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class Deck extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      card: {
-        value: '',
-        name: '',
-        suit: '',
-      },
-      cards: []
-    }
-  }
+// export default class Deck extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       card: {
+//         value: '',
+//         name: '',
+//         suit: '',
+//       },
+//       cards: []
+//     }
+//   }
 
-  deck = (e) => {
-    this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-    this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
-    var card 
-    var cards = [];
+//   deck = (e) => {
+//     this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+//     this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+//     var card 
+//     var cards = [];
 
-    for (var s = 0; s < this.suits.length; s++) {
-      for (var n = 0; n < this.names.length; n++) {
-        cards.push(new card(n + 1, this.names[n], this.suits[s]));
-      }
-    }
+//     for (var s = 0; s < this.suits.length; s++) {
+//       for (var n = 0; n < this.names.length; n++) {
+//         cards.push(new card(n + 1, this.names[n], this.suits[s]));
+//       }
+//     }
 
-    return cards;
-  }
+//     return cards;
+//   }
 
-  render() {
-    console.log(this.state)
+//   render() {
+//     console.log(this.state)
 
-    return (
+//     return (
 
-      <div>
+//       <div>
 
-      </div>
-    )
-  }
-}
+//       </div>
+//     )
+//   }
+// }
 
 
 
-function card(value, name, suit) {
-  this.value = value;
-  this.name = name;
-  this.suit = suit;
-}
+// function card(value, name, suit) {
+//   this.value = value;
+//   this.name = name;
+//   this.suit = suit;
+// }
 
-function deck() {
-  this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
-  var cards = [];
+// function deck() {
+//   this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+//   this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+//   var cards = [];
 
-  for (var s = 0; s < this.suits.length; s++) {
-      for (var n = 0; n < this.names.length; n++) {
-          cards.push(new card(n + 1, this.names[n], this.suits[s]));
-      }
-  }
+//   for (var s = 0; s < this.suits.length; s++) {
+//       for (var n = 0; n < this.names.length; n++) {
+//           cards.push(new card(n + 1, this.names[n], this.suits[s]));
+//       }
+//   }
 
-  return cards;
-}
+//   return cards;
+// }
