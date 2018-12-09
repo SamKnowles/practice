48240feca44818bff2620b7af51185ac090db06e
Sam Knowles <samuelkcknowles@gmail.com>
2018-12-08 21:25:27 -0700

    created player component

---
+++ src/Components/Player.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class Player extends Component {
+    render() {
+        return {
+            name: '',
+            activePlayer: false,
+            chipCount: 200,
+            position: '',
+            hand: [{suit: '', name: ''}],
+            action: {
+                folded: false,
+                raised: false,
+                checked: false,
+                called: false,
+                bet: false
+            },
+
+        };
+    }
+}
+
