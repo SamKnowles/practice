6b437d55dd71d546d537615c890f5f3e85fdb023
Sam Knowles <samuelkcknowles@gmail.com>
2018-12-09 16:05:30 -0700

    deleted useless components and styles

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,19 +2,15 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom';
 
 import Nav from './Nav'
-import Landing from './Landing'
 import PreFlop from './PreFlop'
-import Maths from './Maths'
 import Table from './Table'
 import CallingHands from './CallingHands'
-import Deck from './Deck'
 
 import '../Styles/App.css';
 import '../Styles/PreFlop.css';
 import '../Styles/Table.css';
 import '../Styles/Nav.css';
 import '../Styles/CallingHands.css';
-import '../Styles/Deck.css';
 import '../Styles/Table.css';
 
 function App() {
@@ -24,10 +20,7 @@ function App() {
             <Nav />
             <Switch>
                 <Route exact path='/' component={CallingHands}></Route>
-                <Route path='/Deck' component={Deck}></Route>
-                <Route path='/Landing' compontnet={Landing} ></Route>
                 <Route path='/PreFlop' component={PreFlop}></Route>
-                <Route path='/Maths' component={Maths}></Route>
                 <Route path='/Table' component={Table}/>
             </Switch>
         </div>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,10 +5,6 @@ function Nav() {
     return (
         <div className='nav-container'>
             <NavLink exact className='nav-item' activeClassName="selcted" to='/'>Calling Hands</NavLink>
-            {/* <NavLink className='nav-item' activeClassName="selcted" to='/deck'>Deck</NavLink>
-            <NavLink className='nav-item' activeClassName="selcted" to='/landing'>Landing</NavLink>
-            <NavLink className='nav-item' activeClassName="selcted" to='/preflop'>Pre-Flop</NavLink>
-            <NavLink className='nav-item' activeClassName="selcted" to='/maths'>Maths</NavLink> */}
             <NavLink className='nav-item' activeClassName="selcted" to='/table'>Table</NavLink>
         </div>
     )
