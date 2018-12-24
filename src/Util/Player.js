f780fa845c302cb420b0546c0993ed90442cb45f
Sam Knowles <samuelkcknowles@gmail.com>
2018-12-23 21:17:42 -0700

    started added some utilities

---
+++ src/Util/Player.js
@@ -0,0 +1,21 @@
+export default class Player {
+        constructor(name, activePlayer, chipCount, position) {
+            this.name = '';
+            this.activePlayer = false;
+            this.chipCount = 200;
+            this.position = '';
+            this.hand = [{suit: '', name: ''}];
+            this.action = {
+                folded: false,
+                raised: false,
+                checked: false,
+                called: false,
+                bet: false
+            };
+        }
+
+        takeTurn(ation) {
+            
+        }
+}
+
