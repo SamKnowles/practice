f9e8c3066c2bdff1095b0688a0895e2c29425e2d
Sam Knowles <samuelkcknowles@gmail.com>
2018-12-27 14:36:54 -0700

    added playersoptions component

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -165,21 +165,6 @@ function Table() {
                             <div className='player-chips'><img className='chips-pic' src={chipPic} alt="" /></div>
                             <div className='player-face'><img className='playerFace' src={putin} alt="" /></div>
                         </div>
-                        {/* <div className='player-options-wrapper'>
-                            <div className='fold-button'>Fold</div>
-                            <div className='bet-raise-wrapper'>
-                                <div className='slider-wrapper'>
-                                    <div className='raise-amount'>$98,000</div>
-                                    <div className='slider'>Money Slider</div>
-                                </div>
-                                <div className='bet-raise-button-wrappers'>
-                                    <div className='bet-button'>Bet</div>
-                                    <div className='raise-button'>Raise</div>
-                                </div>
-                            </div>
-                            <div className='check-button'>Check</div>
-                            <div className='call-button'>Call</div>
-                        </div> */}
                     </div>
                 </div>
             </div>
